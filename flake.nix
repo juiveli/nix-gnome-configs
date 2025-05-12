dde5594e10f7680ea02a3421fcc3523e9c7ea8a7
joonas <j00nas.makinen@hotmail.com>
2025-05-12 18:21:46 +0300

    fixed dependency of nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "Manage flatpak apps declaratively.";
 
-  outputs = _:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, nixpkgs }:
     {
       homeManagerModules = { nix-gnome-home-configs = import ./modules/gnome.nix; };;
     };
